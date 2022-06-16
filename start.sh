e538576a3ed138ea3ad3b0e31c4ba1e65485324f
REDSEC <68374733+hjprihatini@users.noreply.github.com>
2022-06-16 12:10:59 +0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+clear
+figlet Pempes
+echo "Version 2.0"
+echo "==========================="
+echo "Admin : RedSec"
+echo "Team : MedanSec | LIT"
+echo "Copyright © Upload File"
+echo "==========================="
+echo
+echo "What Is Your Name Bro?"
+read Bro
+echo "[==============================]"
+echo "Hai $Bro Welcome Tools. Happy Deface"
+echo "[==============================]"
+echo "Loading..."
+sleep 4
+echo "<=====================>"
+echo "Succses"
+sleep 1
+echo
+echo "Link : https://airsense.com/en/contact/service"
+sleep 1
+echo "Link : https://iberoamericajournal.ru/en/send-article.html-0"
+sleep 1
+echo "Link : https://www.pohkong.com.my/proposal"
+sleep 1
+echo "link : https://ysu.edu/human-resources/position-change-form"
+sleep 1
+echo "Link : http://csrgardens.in/?q=Contact"
+sleep 1
+echo "Link : https://www.smithcoffey.com.au/upload-files/"
+sleep 1
+echo "Link : https://www.somerville.org.au/typography/sub-page-webfor>"
+sleep 1
+echo "Link : http://www.twk.org.za/career-application"
+sleep 1
+echo "Link : https://www.gilstermarylee.com/file-upload?wssl=1"
+echo
+sleep 3
+echo "Thx Bro :)"
+echo
